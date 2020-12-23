14035ae41e50e052724a28d86cbdf551e55c7859
Amanuel Bitsuamlak <amanbitsu34@gmail.com>
2020-12-23 12:10:29 -0800

    The user sends Hi and the bot Starts Replying...

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using Telegram.Bot;
+using System.IO;
+using Telegram.Bot.Types.InputFiles;
+
 
 namespace TelegramGAPIBot
 {
     class Program
     {
+        static TelegramBotClient botClient = new TelegramBotClient("1338176434:AAHj44O4diyNRxeZNH2P12qIX7A-_EIDpsI");
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            botClient.OnMessage += BotClient_OnMessage;
+            botClient.StartReceiving();
+            Console.ReadLine();
+        }
+
+        private static void BotClient_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            Console.WriteLine(e.Message.Text);
+
+
+            if (e.Message.Text == "Hi")
+            {
+                botClient.SendTextMessageAsync(e.Message.Chat.Id, "What's Up ?");
+            }
+        }
         }
     }
-}
