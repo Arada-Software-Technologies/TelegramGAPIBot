f834387d688e15b150472aab92127c6facc79ec2
Amanuel Bitsuamlak <amanbitsu34@gmail.com>
2020-12-25 10:28:09 -0800

    telegram bot developed Fixes #4

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using Telegram.Bot;
-using System.IO;
-using Telegram.Bot.Types.InputFiles;
-
+//using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.ReplyMarkups;
+//using Telegram.Bot.Types;
+//using Telegram.Bot.Types.Enums;
+//using System.Net.Http;
+//using System.Threading.Tasks;
+//using Google.Cloud.Vision.V1;
 
 namespace TelegramGAPIBot
 {
@@ -25,6 +29,17 @@ namespace TelegramGAPIBot
             {
                 botClient.SendTextMessageAsync(e.Message.Chat.Id, "What's Up ?");
             }
+            // Defining Keyboard button that requests a contact
+            KeyboardButton button = KeyboardButton.WithRequestContact("Send contact");
+
+            //Defining Keyboard that contains the button
+            ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup(button);
+
+            // Send keyboard with message!
+             botClient.SendTextMessageAsync(e.Message.Chat, "Please send contact", replyMarkup: keyboard);
+
+
         }
-        }
+        
     }
+}
